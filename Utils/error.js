8c2b15e9c1fedeaee2194c2e893f5f65cbe5daf3
Shyam <ntshyamsunder96@gmail.com>
2025-01-26 21:07:12 +0530

    Clear cache

--- Utils/error.js
+++
@@ -1,6 +0,0 @@
-export const errorHandler = (statusCode, message) => {
-    const error = new Error(); // Use the built-in Error constructor
-    error.statusCode = statusCode;
-    error.message = message;
-    return error;
-};
